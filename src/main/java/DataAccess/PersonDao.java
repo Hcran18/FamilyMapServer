e80959f86102c842af67ba448d497e30b6e2cc8e
Hcran18 <hunter.crandall18@gmail.com>
2023-05-23 13:28:25 -0600

    PersonDao

--- src/main/java/DataAccess/PersonDao.java
+++ src/main/java/DataAccess/PersonDao.java
@@ -3,11 +3,48 @@ package DataAccess;
 import model.Person;
 
 import java.sql.*;
+import java.util.*;
 
 public class PersonDao {
     private final Connection conn;
 
     public PersonDao(Connection conn) {
         this.conn = conn;
+    }
+
+    public void createPerson(Person person) {
+
+    }
+
+    public Person findByID(String personID) {
+        return null;
+    }
+
+    public List<Person> findAllForUser(String associatedUsername) {
+        return Collections.emptyList();
+    }
+
+    public List<Person> findParents(String personID) {
+        return Collections.emptyList();
+    }
+
+    public Person findSpouse(String personID) {
+        return null;
+    }
+
+    public List<Person> findChildren(String personID) {
+        return Collections.emptyList();
+    }
+
+    public void update(Person person) {
+
+    }
+
+    public void delete(Person person) {
+
+    }
+
+    public void deleteByID(String personID) {
+
     }
 }
