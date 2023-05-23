fdd94edf889a344d901e088c67a25ceb889cefe6
Hcran18 <hunter.crandall18@gmail.com>
2023-05-23 12:17:53 -0600

    User Class

---
+++ src/main/java/model/User.java
@@ -0,0 +1,77 @@
+package model;
+
+public class User {
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String personID;
+
+    public User(String username, String password, String email, String firstName, String lastName, String gender, String personID) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.personID = personID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPersonID() {
+        return personID;
+    }
+
+    public void setPersonID(String personID) {
+        this.personID = personID;
+    }
+}
