4aafe311c4c7bbc15ddf27072734e05ce3d253f1
Hcran18 <hunter.crandall18@gmail.com>
2023-05-24 10:32:55 -0600

    Requests

---
+++ src/main/java/Request/LoadRequest.java
@@ -0,0 +1,35 @@
+package Request;
+
+import model.User;
+import model.Person;
+import model.Event;
+
+public class LoadRequest {
+    User[] users;
+    Person[] persons;
+    Event[] events;
+
+    public User[] getUsers() {
+        return users;
+    }
+
+    public void setUsers(User[] users) {
+        this.users = users;
+    }
+
+    public Person[] getPersons() {
+        return persons;
+    }
+
+    public void setPersons(Person[] persons) {
+        this.persons = persons;
+    }
+
+    public Event[] getEvents() {
+        return events;
+    }
+
+    public void setEvents(Event[] events) {
+        this.events = events;
+    }
+}
---
+++ src/main/java/Request/RegisterRequest.java
@@ -0,0 +1,58 @@
+package Request;
+
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String gender;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
