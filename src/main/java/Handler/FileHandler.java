4e51b6b1021b75254ce8fed206eae1ce3c45e2fa
Hcran18 <hunter.crandall18@gmail.com>
2023-05-30 16:53:52 -0600

    Server and FileHandler

---
+++ src/main/java/Handler/FileHandler.java
@@ -0,0 +1,54 @@
+package Handler;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.nio.file.Files;
+
+public class FileHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+        try {
+            if (exchange.getRequestMethod().toLowerCase().equals("get")) {
+                String urlPath = exchange.getRequestURI().toString();
+
+                if (urlPath == null || urlPath.equals("/")) {
+                    urlPath = "/index.html";
+                }
+
+                String filePath = "web" + urlPath;
+
+                File file = new File(filePath);
+                if (file.exists()) {
+                    exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+
+                    OutputStream respBody = exchange.getResponseBody();
+                    Files.copy(file.toPath(), respBody);
+                    respBody.close();
+                }
+                else {
+                    // Return 404 error (File Not Found)
+                    exchange.sendResponseHeaders(404, 0);
+                    OutputStream respBody = exchange.getResponseBody();
+                    respBody.close();
+                }
+            }
+            else {
+                // Return 405 error (Method Not Allowed)
+                exchange.sendResponseHeaders(405, 0);
+                OutputStream respBody = exchange.getResponseBody();
+                respBody.close();
+            }
+        }
+        catch (IOException e) {
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_SERVER_ERROR, 0);
+            exchange.getResponseBody().close();
+            e.printStackTrace();
+        }
+    }
+}
